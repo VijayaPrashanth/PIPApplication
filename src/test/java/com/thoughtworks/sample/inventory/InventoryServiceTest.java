39242ceeb4d052c1c4db727603922474340a7a2a
VijayaPrashanth <vijaya.prashanth500@gmail.com>
2023-09-08 07:19:52 +0530

    Refactor. checked with existing tests in inventoryService

--- src/test/java/com/thoughtworks/sample/inventory/InventoryServiceTest.java
+++ src/test/java/com/thoughtworks/sample/inventory/InventoryServiceTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.sample.inventory;
 
+import com.thoughtworks.sample.exception.ItemNotFoundException;
 import com.thoughtworks.sample.inventory.repository.Inventory;
 import com.thoughtworks.sample.inventory.repository.InventoryRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,7 +32,7 @@ public class InventoryServiceTest {
     @Test
     public void shouldReturnItemList() {
         List<Inventory> inventoryList = new ArrayList<>();
-        Inventory item = new Inventory("onion",new BigDecimal(40),"KG");
+        Inventory item = new Inventory("onion",new BigDecimal(40),"1KG");
         inventoryList.add(item);
 
         when(inventoryRepository.findAll()).thenReturn(inventoryList);
@@ -41,5 +42,36 @@ public class InventoryServiceTest {
         assertEquals(inventoryList, itemList);
     }
 
+    @Test
+    public void shouldAddItemToInventory() {
+        Inventory item = new Inventory("onion",new BigDecimal(40),"1KG");
 
+        when(inventoryRepository.save(item)).thenReturn(item);
+        inventoryService.addItems(item);
+
+        verify(inventoryRepository,times(1)).save(item);
+    }
+
+    @Test
+    public void shouldDeleteItemFromInventory() throws ItemNotFoundException {
+        int id = 1;
+
+        when(inventoryRepository.existsById(id)).thenReturn(true);
+        String result = inventoryService.deleteItem(id);
+
+        verify(inventoryRepository, times(1)).deleteById(id);
+        assertEquals("Product removed from Inventory", result);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenIdIsNotFoundForDeletion() {
+        int id = 1;
+
+        when(inventoryRepository.existsById(id)).thenReturn(false);
+        assertThrows(ItemNotFoundException.class, () -> {
+            inventoryService.deleteItem(id);
+        });
+
+        verify(inventoryRepository, never()).deleteById(id);
+    }
 }
