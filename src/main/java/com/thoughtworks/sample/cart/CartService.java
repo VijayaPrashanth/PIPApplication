9bbc42f1bc50128474cbc158b10b1b7df8317fcd
VijayaPrashanth <vijaya.prashanth500@gmail.com>
2023-09-08 06:33:09 +0530

    Refactor. modified cartService with relation to inventory with tests passing

--- src/main/java/com/thoughtworks/sample/cart/CartService.java
+++ src/main/java/com/thoughtworks/sample/cart/CartService.java
@@ -3,10 +3,13 @@ package com.thoughtworks.sample.cart;
 import com.thoughtworks.sample.cart.repository.Cart;
 import com.thoughtworks.sample.cart.repository.CartRepository;
 import com.thoughtworks.sample.exception.ItemNotFoundException;
+import com.thoughtworks.sample.inventory.repository.Inventory;
+import com.thoughtworks.sample.inventory.repository.InventoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartService {
@@ -14,13 +17,22 @@ public class CartService {
     @Autowired
     private CartRepository cartRepository;
 
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
     public CartService(CartRepository cartRepository) {
         this.cartRepository = cartRepository;
     }
 
-    public List<Cart> addItems(Cart cart) {
+    public List<Cart> addItems(Cart cart) throws ItemNotFoundException {
 
-        cartRepository.save(cart);
+        if(!inventoryRepository.existsById(cart.getId()))
+            throw new ItemNotFoundException();
+        Optional<Inventory> inventoryOptional = inventoryRepository.findById(cart.getId());
+        Inventory inventory = inventoryOptional.get();
+        Cart cart1 = new Cart(inventory, cart.getName(), cart.getQuantity(), cart.getUnit());
+
+        cartRepository.save(cart1);
         return cartRepository.getItemDetails();
     }
 
