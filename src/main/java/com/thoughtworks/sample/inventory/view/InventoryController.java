1d2a2e4d5d591cc249772539f4526c6c78d808ef
VijayaPrashanth <vijaya.prashanth500@gmail.com>
2023-09-08 07:41:02 +0530

    Refactor.added endpoint for delete with passing tests

--- src/main/java/com/thoughtworks/sample/inventory/view/InventoryController.java
+++ src/main/java/com/thoughtworks/sample/inventory/view/InventoryController.java
@@ -1,11 +1,13 @@
 package com.thoughtworks.sample.inventory.view;
 
+import com.thoughtworks.sample.exception.ItemNotFoundException;
+import com.thoughtworks.sample.handlers.ErrorResponse;
 import com.thoughtworks.sample.inventory.InventoryService;
 import com.thoughtworks.sample.inventory.repository.Inventory;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +27,17 @@ public class InventoryController {
         return inventoryService.getItemList();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Fetched admin details successfully"),
+            @ApiResponse(code = 404, message = "Record not found", response = ErrorResponse.class),
+            @ApiResponse(code = 500, message = "Something failed in the server", response = ErrorResponse.class)
+    })
+    @PutMapping("/add")
+    public List<Inventory> addItemsToInventory(@RequestBody Inventory inventory)  {
+        return inventoryService.addItems(inventory);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteItemFromCart(@PathVariable int id) throws ItemNotFoundException {
+        return inventoryService.deleteItem(id);
+    }
 }
